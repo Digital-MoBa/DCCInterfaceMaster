f7305cad5aca0484f458cf3d17186552f9a014b9
Philipp Gahtow <Digital-MoBa@users.noreply.github.com>
2021-04-19 14:13:56 +0200

    add half one bit

--- DDCHardware_config.h
+++ DDCHardware_config.h
@@ -4,7 +4,7 @@
 * DCC Waveform Timer Configuration
 *
 * DCC Master Interface can generate DCC Signal 
-* either with Timer1 (16-bit) or with Timer2 (8-bit)
+* either with Timer1 (16-bit) or with Timer2 (8-bit) by default!
 * for ARM on Arduino DUE with TC3 = Timer4 (TC1 Channel 0)
 * for ESP8266 with Timer1
 * for ESP32 with Timer1
@@ -57,6 +57,7 @@ Dauer des Teil-Nullbits: t ≥ 100 µs, normal: 116µs
 /******************************************/
 //Arduino DUE DCC Signal generation on TC3 = Timer4 (TC1 Channel 0)
 #if defined(__SAM3X8E__)
+#define half_one_count 305  //29usec pulse
 #define one_count	609		// Calls every 58µs
 #define zero_high_count	1050	// Calls every 100µs
 //#define zero_low_count	1050	// Calls every 100µs
@@ -64,6 +65,7 @@ Dauer des Teil-Nullbits: t ≥ 100 µs, normal: 116µs
 #define DCC_ARM_TC_CHANNEL	0			//Channel
 #define DCC_ARM_MATCH_INT	TC6_IRQn	//Interrupt that will be used
 #define DCC_ARM_TC_SIGNAL	TC6_Handler()	//Interrupt Handler
+#define DCC_TMR_OUTP_ONE_HALF()  {TC_SetRC(DCC_ARM_TC_TIMER, DCC_ARM_TC_CHANNEL, half_one_count); }	
 #define DCC_TMR_OUTP_ONE_COUNT() {TC_SetRC(DCC_ARM_TC_TIMER, DCC_ARM_TC_CHANNEL, one_count); }	
 #define DCC_TMR_OUTP_ZERO_HIGH() {TC_SetRC(DCC_ARM_TC_TIMER, DCC_ARM_TC_CHANNEL, zero_high_count);}    
 //#define DCC_TMR_OUTP_ZERO_LOW()  {TC_SetRC(DCC_ARM_TC_TIMER, DCC_ARM_TC_CHANNEL, zero_low_count);}    
@@ -74,12 +76,14 @@ Dauer des Teil-Nullbits: t ≥ 100 µs, normal: 116µs
 //TIM_DIV1 = 0   -> 80MHz (80 ticks/us - 104857.588us max)
 //TIM_DIV16 = 1  -> 5MHz (5 ticks/us - 1677721.4us max)
 //TIM_DIV256 = 3 -> 312.5Khz (1 tick = 3.2us - 26843542.4us max)
+#define half_one_count 145  //29usec pulse
 #define one_count 290		// 290 - Calls every 58µs
 #define zero_high_count	501	// 500 - Calls every 100µs
 #define zero_low_count	501	// Calls every 100µs
 #define DCC_ESP_TIMER_DIV 		TIM_DIV16
 #define DCC_ESP_TIMER_SET		TIM_EDGE
 #define DCC_ESP_TIMER_LOOP  	TIM_SINGLE
+#define DCC_TMR_OUTP_ONE_HALF() {timer1_write(half_one_count); last_timer = half_one_count;}	
 #define DCC_TMR_OUTP_ONE_COUNT() {timer1_write(one_count); last_timer = one_count;}	
 #define DCC_TMR_OUTP_ZERO_HIGH() {timer1_write(zero_high_count); last_timer = zero_high_count;}    
 #define DCC_TMR_OUTP_ZERO_LOW()  {timer1_write(zero_low_count); last_timer = zero_low_count;}    
@@ -87,11 +91,13 @@ Dauer des Teil-Nullbits: t ≥ 100 µs, normal: 116µs
 /******************************************/
 //ESP32 DCC Signal generation with Timer1
 #elif defined(ESP32)
+#define half_one_count 29  //29usec pulse
 #define one_count 58		// 290 - Calls every 58µs
 #define zero_high_count	100	// 500 - Calls every 100µs
 #define zero_low_count	100	// Calls every 100µs
 #define DCC_ESP_TIMER_PRESCALE	80		//prescale the value of the time divider
 #define DCC_ESP_TIMER_FLAG		true	//flag true to count on the rising edge, false to count on the falling edge
+#define DCC_TMR_OUTP_ONE_HALF() {timerAlarmWrite(timer, half_one_count, true); last_timer = half_one_count;}	
 #define DCC_TMR_OUTP_ONE_COUNT() {timerAlarmWrite(timer, one_count, true); last_timer = one_count;}	
 #define DCC_TMR_OUTP_ZERO_HIGH() {timerAlarmWrite(timer, zero_high_count, true); last_timer = zero_high_count;}    
 #define DCC_TMR_OUTP_ZERO_LOW()  {timerAlarmWrite(timer, zero_low_count, true); last_timer = zero_low_count;} 
@@ -101,6 +107,7 @@ Dauer des Teil-Nullbits: t ≥ 100 µs, normal: 116µs
 #elif defined(DCC_USE_TIMER1)
 #undef DCC_USE_TIMER2
 
+#define half_one_count 57  //29usec pulse
 #define one_count 115 //58us = 115
 #define zero_high_count 199 //100us = 199	!old: 116us = 228
 #define zero_low_count 199  //100us
@@ -114,6 +121,7 @@ Dauer des Teil-Nullbits: t ≥ 100 µs, normal: 116µs
 #define DCC_TMR_MATCH_INT()    {TIMSK1 |= (1 << OCIE1A);} //Compare Match Interrupt Enable
 #define DCC_TMR_OUTP_CAPT_REG  OCR1A
 
+#define DCC_TMR_OUTP_ONE_HALF()  {OCR1A = OCR1B = half_one_count;}
 #define DCC_TMR_OUTP_ONE_COUNT() {OCR1A = OCR1B = one_count;}
 #define DCC_TMR_OUTP_ZERO_HIGH() {OCR1A = OCR1B = zero_high_count;}
 #define DCC_TMR_OUTP_ZERO_LOW()  {OCR1A = OCR1B = zero_low_count;}
@@ -123,6 +131,7 @@ Dauer des Teil-Nullbits: t ≥ 100 µs, normal: 116µs
 #else
 #define DCC_USE_TIMER2
 
+#define half_one_count 198  //29usec pulse
 #define one_count 141  //58usec pulse length = 141 = 0x8D
 #define zero_high_count 56  //100us = 56		!old: 116us = 0x1B (27) pulse length
 #define zero_low_count 56  //100us
@@ -137,6 +146,7 @@ Dauer des Teil-Nullbits: t ≥ 100 µs, normal: 116µs
 #define DCC_TMR_MATCH_INT()    {TIMSK2 = 1 << TOIE2;} //Overflow Interrupt Enable
 
 //note that there is a latency so take the last time of Timer2 also:
+#define DCC_TMR_OUTP_ONE_HALF()  {DCC_TMR_COUNT_REG = DCC_TMR_COUNT_REG + half_one_count; last_timer = half_one_count;}
 #define DCC_TMR_OUTP_ONE_COUNT() {DCC_TMR_COUNT_REG = DCC_TMR_COUNT_REG + one_count; last_timer = one_count;}
 #define DCC_TMR_OUTP_ZERO_HIGH() {DCC_TMR_COUNT_REG = DCC_TMR_COUNT_REG + zero_high_count; last_timer = zero_high_count;}
 #define DCC_TMR_OUTP_ZERO_LOW() {DCC_TMR_COUNT_REG = DCC_TMR_COUNT_REG + zero_low_count; last_timer = zero_low_count;}
